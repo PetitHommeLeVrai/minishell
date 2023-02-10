b742aec4a619e99a18552b0660e7ddb66cd36f0c
aboyer <118172356+PetitHommeLeVrai@users.noreply.github.com>
2023-02-10 14:15:45 +0100

    oops changed wrong files

--- executions/execs_files.c
+++ executions/execs_files.c
@@ -6,64 +6,67 @@
 /*   By: aboyer <aboyer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 10:43:38 by aboyer            #+#    #+#             */
-/*   Updated: 2023/02/06 12:13:20 by aboyer           ###   ########.fr       */
+/*   Updated: 2023/02/08 11:30:29 by aboyer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tmp.h"
 
-void	infile(t_token *word, t_cmd_line *cmd_line)
+void	infile(char *word, t_cmd_line *cmd_line)
 {
 	if (cmd_line->infile)
 		close(cmd_line->infile);
-	cmd_line->infile = open(word->word, O_RDONLY);
+	cmd_line->infile = open(word, O_RDONLY);
 	if (cmd_line->infile == -1)
 	{
-		perror(word->word);
+		perror(word);
 		exit(2);
 	}
 }
 
-void	outfile(t_token *word, t_cmd_line *cmd_line)
+void	outfile(char *word, t_cmd_line *cmd_line)
 {
 	if (cmd_line->outfile)
 		close(cmd_line->outfile);
-	cmd_line->outfile = open(word->word, O_TRUNC | O_CREAT | O_RDWR, 0000644);
+	cmd_line->outfile = open(word, O_TRUNC | O_CREAT | O_RDWR, 0000644);
 	if (cmd_line->outfile == -1)
 	{
-		perror(word->word);
+		perror(word);
 		exit(2);
 	}
 }
 
-void	outfileover(t_token *word, t_cmd_line *line)
+void	outfileover(char *word, t_cmd_line *line)
 {
 	if (line->outfile)
 		close(line->outfile);
-	line->outfile = open(word->word, O_WRONLY | O_CREAT | O_APPEND, 0000644);
+	line->outfile = open(word, O_WRONLY | O_CREAT | O_APPEND, 0000644);
 	if (line->outfile == -1)
 	{
-		perror(word->word);
+		perror(word);
 		exit(2);
 	}
 }
 
 void	get_files(t_exec *exec, t_cmd_line *cmd_line)
 {
-	while (cmd_line->word != NULL)
+	int	i;
+
+	i = 0;
+	while (i < cmd_line->token_list->count)
 	{
-		if (cmd_line->word->type == INFILE)
-			infile(cmd_line->word, cmd_line);
-		else if (cmd_line->word->type == OUTFILE)
-			outfile(cmd_line->word, cmd_line);
-		else if (cmd_line->word == OUTFILEOVER)
-			outfileover(cmd_line->word, cmd_line);
-		else if (cmd_line->word == LIMITOR)
+		if (cmd_line->token_list->token[i].type == INFILE)
+			infile(cmd_line->token_list->token[i].word, cmd_line);
+		else if (cmd_line->token_list->token[i].type == OUTFILE)
+			outfile(cmd_line->token_list->token[i].word, cmd_line);
+		else if (cmd_line->token_list->token[i].type == OUTFILEOVER)
+			outfileover(cmd_line->token_list->token[i].word, cmd_line);
+		else if (cmd_line->token_list->token[i].type == LIMITOR)
 		{
 			if (cmd_line->infile)
 				close(cmd_line->infile);
-			heredoc(cmd_line->word->word, exec);
+			heredoc(cmd_line->token_list->token[i].word, exec);
 		}
-		cmd_line->word = cmd_line->word->next;
+		i++;
 	}
 }
